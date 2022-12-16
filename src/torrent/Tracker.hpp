83105ca83471baefdff93ddc8c5de713b7a2281c
yavor2 <yavor.s.pachedzhiev.2018@elsys-bg.org>
2022-12-17 00:37:43 +0200

    changes

--- src/torrent/Tracker.hpp
+++ src/torrent/Tracker.hpp
@@ -1,6 +1,20 @@
 #ifndef __TORRENT_TRACKER_H
 #define __TORRENT_TRACKER_H
+#include <string>
+#include <boost/asio.hpp>
 
 
+class Tracker{
+private:
+    std::string url;
+    std::string port;
+    boost::asio::io_context context;
+public:
+    Tracker(std::string url);
+    inline std::string get_url() const { return this->url; }
+    inline std::string get_port() const { return this->port; }
+    
+    void query();
+};
 
 #endif
