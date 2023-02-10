c5bf7dbc95a302c5528c9fbe2bfd7f610db5ad01
yavor2 <yavor.s.pachedzhiev.2018@elsys-bg.org>
2023-02-11 00:57:56 +0200

    set up peer handling logic

--- src/torrent/peer.cpp
+++ src/torrent/peer.cpp
@@ -1,10 +1,88 @@
-#include "Peer.hpp"
+#include "peer.h"
+#include "torrent.h"
+#include <vector>
+#include <iostream>
 
-
-
-void Peer::connect()
+Peer::Peer(Torrent *torrent)
+	: m_torrent(torrent)
 {
-    
+	m_conn = new Connection();
+	m_state = PS_AmChoked | PS_PeerChoked;
+}
+
+Peer::~Peer()
+{
+	delete m_conn;
+	for (Piece *p : m_queue)
+		delete p;
+	m_queue.clear();
+}
+void Peer::connect(const std::string &ip, const std::string &port)
+{
+	m_conn->setErrorCallback(std::bind(&Peer::handleError, shared_from_this(), std::placeholders::_1));
+	m_conn->connect(ip, port,
+		[this] ()
+		{
+			const uint8_t *m_handshake = m_torrent->handshake();
+			m_conn->write(m_handshake, 68);
+			m_conn->read_partial(68,
+				[this, m_handshake] (const uint8_t *handshake, size_t size)
+				{
+					if (size != 68
+						|| (handshake[0] != 0x13 && memcmp(&handshake[1], "BitTorrent protocol", 19) != 0)
+						|| memcmp(&handshake[28], &m_handshake[28], 20) != 0)
+						return handleError("info hash/protocol type mismatch");
+
+					std::string peerId((const char *)&handshake[48], 20);
+					if (!m_peerId.empty() && peerId != m_peerId)
+						return handleError("unverified");
+
+					m_peerId = peerId;
+					m_torrent->addPeer(shared_from_this());
+					m_conn->read_partial(4, std::bind(&Peer::handle, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
+				}
+			);
+		}
+	);
+}
+
+
+void Peer::handle(const uint8_t *data, size_t size)
+{
+	if (size != 4)
+		return handleError("Peer::handle(): Expected 4-byte length");
+
+	uint32_t length = readAsBE32(data);
+	switch (length) {
+	case 0: // Keep alive
+		return m_conn->read(4, std::bind(&Peer::handle, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
+	default:
+		m_conn->read(length,
+			[this] (const uint8_t *data, size_t size)
+			{
+				InputMessage in(const_cast<uint8_t *>(&data[1]), size - 1);
+				handleMessage((MessageType)data[0], in);
+			}
+		);
+	}
+}
+void Peer::handleMessage(MessageType messageType, InputMessage in)
+{
+	size_t payloadSize = in.getSize();
+
+	switch (messageType) {
+	case MT_Choke:
+	case MT_UnChoke:
+	case MT_Interested:
+	case MT_NotInterested:
+	case MT_Bitfield:
+	case MT_Request:
+	case MT_PieceBlock:
+	case MT_Cancel:
+	case MT_Port:
+	}
 
 }
+
+
 
--- src/torrent/peer.h
+++ src/torrent/peer.h
@@ -1,19 +1,76 @@
 #ifndef PEER_H
 #define PEER_H
-#include <string>
 
+#include <net/connection.h>
+#include <net/inputmessage.h>
+
+#include <memory>
+#include <vector>
 
 class Torrent;
-class Peer
+class Peer : public std::enable_shared_from_this<Peer>
 {
-private:
-    std::string ip;
-    std::string port;
-    Torrent *torr;
-    
+	struct Piece;
+	enum State : uint8_t {
+		PS_AmChoked = 1 << 0,			// We choked this peer (aka we're not giving him anymore pieces)
+		PS_AmInterested = 1 << 1,		// We're interested in this peer's pieces
+		PS_PeerChoked = 1 << 2,			// Peer choked us
+		PS_PeerInterested = 1 << 3,		// Peer interested in our stuff
+	};
+
+	enum MessageType : uint8_t {
+		MT_Choke		= 0,
+		MT_UnChoke		= 1,
+		MT_Interested		= 2,
+		MT_NotInterested	= 3,
+		MT_Have			= 4,
+		MT_Bitfield		= 5,
+		MT_Request		= 6,
+		MT_PieceBlock		= 7,
+		MT_Cancel		= 8,
+		MT_Port			= 9
+	};
+
 public:
-    Peer(Torrent *t) : torr(t){};
-    void connect();
+	Peer(Torrent *t);
+	~Peer();
+
+	inline void setId(const std::string &id) { m_peerId = id; }
+	inline std::string getIP() const { return m_conn->getIPString(); }
+	inline uint32_t ip() const { return m_conn->getIP(); }
+	void disconnect();
+	void connect(const std::string &ip, const std::string &port);
+
+protected:
+	void handle(const uint8_t *data, size_t size);
+	void handleMessage(MessageType mType, InputMessage in);
+
+private:
+	struct PieceBlock {
+		size_t size;
+		uint8_t *data;
+
+		PieceBlock() { data = nullptr; size = 0; }
+		~PieceBlock() { delete []data; }
+	};
+
+	struct Piece {
+		size_t index;
+		size_t currentBlocks;
+		size_t numBlocks;
+
+		~Piece() { delete []blocks; }
+		PieceBlock *blocks;
+	};
+
+	std::string m_peerId;
+	uint8_t m_state;
+	Torrent *m_torrent;
+	Connection *m_conn;
+
+	friend class Torrent;
 };
 
+
 #endif
+
