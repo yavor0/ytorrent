67e8a623b07791142d60a2b11df156946792a515
yavor2 <yavor.s.pachedzhiev.2018@elsys-bg.org>
2023-02-15 05:29:07 +0200

    finish download strategy

--- src/torrent/torrent.cpp
+++ src/torrent/torrent.cpp
@@ -1,127 +1,200 @@
 #include "torrent.hpp"
-
 #include <net/connection.hpp>
 #include <utils/utils.hpp>
-#include <random>
 #include <algorithm>
 #include <iostream>
 #include <thread>
-
 #include <boost/uuid/detail/sha1.hpp>
 using namespace bencoding;
 
-
-Torrent::Torrent()
-	: m_completedPieces(0),
-	  m_uploadedBytes(0),
-	  m_downloadedBytes(0),
-	  m_wastedBytes(0),
-	  m_hashMisses(0),
-	  m_pieceLength(0),
-	  m_totalSize(0)
+Torrent::Torrent() : completedPieces(0),
+					 uploadedBytes(0),
+					 downloadedBytes(0),
+					 wastedBytes(0),
+					 hashMisses(0),
+					 pieceLength(0),
+					 totalSize(0)
 {
-
 }
 
 Torrent::~Torrent()
 {
-	for (auto f : m_files)
-		fclose(f.fp);
-	m_files.clear();
-	m_activeTrackers.clear();
+	for (const auto &f : files)
+	{
+		if (f.fp != nullptr)
+		{
+			fclose(f.fp);
+		}
+	}
 }
 
-bool Torrent::open(const std::string &fileName, const std::string &downloadDir)
+bool Torrent::parseFile(const std::string &fileName, const std::string &downloadDir)
 {
-    std::shared_ptr<BItem> decodedData;
-    std::ifstream input(fileName);
-    decodedData = bencoding::decode(input);
-    std::shared_ptr<BDictionary> torrMetaDict = decodedData->as<BDictionary>();
+	std::shared_ptr<BItem> decodedData;
+	std::ifstream input(fileName);
+	decodedData = bencoding::decode(input);
+	std::shared_ptr<BDictionary> torrMetaDict = decodedData->as<BDictionary>();
 
-    std::shared_ptr<BItem> &annVal = (*torrMetaDict)[BString::create("announce")]; //
-    std::shared_ptr<BString> bStrAnn = annVal->as<BString>();
-    m_mainTracker = bStrAnn->value();
+	std::shared_ptr<BItem> &annVal = (*torrMetaDict)[BString::create("announce")]; //
+	std::shared_ptr<BString> bStrAnn = annVal->as<BString>();
+	mainTrackerUrl = bStrAnn->value();
 
 	std::shared_ptr<BItem> &infoVal = (*torrMetaDict)[BString::create("info")];
-    std::shared_ptr<BDictionary> infoDict = infoVal->as<BDictionary>();
+	std::shared_ptr<BDictionary> infoDict = infoVal->as<BDictionary>();
 
 	uint8_t checkSum[20];
-    std::string strInfoHash = bencoding::encode(infoDict);
-    boost::uuids::detail::sha1 sha1;
-    sha1.process_bytes(strInfoHash.data(), strInfoHash.size());
+	std::string strInfoHash = bencoding::encode(infoDict);
+	boost::uuids::detail::sha1 sha1;
+	sha1.process_bytes(strInfoHash.data(), strInfoHash.size());
 
 	unsigned int sum[5];
-    sha1.get_digest(sum);
+	sha1.get_digest(sum);
 	for (int i = 0; i < 5; ++i)
+	{
 		writeAsBE32(&checkSum[i * 4], sum[i]);
-	
-	
-	m_handshake[0] = 0x13;					// 19 length of string "BitTorrent protocol"
-	memcpy(&m_handshake[1], "BitTorrent protocol", 19);
-	memset(&m_handshake[20], 0x00, 8);			// reserved bytes	(last |= 0x01 for DHT or last |= 0x04 for FPE)
-	memcpy(&m_handshake[28], checkSum, 20);			// info hash
-	memcpy(&m_handshake[48], "-YT00000", 8);		// Azureus-style peer id (-CT0000-XXXXXXXXXXXX)
-	memcpy(&m_peerId[0], "-YT00000", 8);
+	}
 
-	static std::random_device rd;
-	static std::ranlux24 generator(rd());
-	static std::uniform_int_distribution<uint8_t> random(0x00, 0xFF);
-	for (size_t i = 8; i < 20; ++i)
-		m_handshake[56 + i] = m_peerId[i] = random(generator);
+	handshake[0] = 0x13; // 19 length of string "BitTorrent protocol"
+	memcpy(&handshake[1], "BitTorrent protocol", 19);
+	memset(&handshake[20], 0x00, 8);	  // reserved bytes	(last |= 0x01 for DHT or last |= 0x04 for FPE)
+	memcpy(&handshake[28], checkSum, 20); // info hash
+	memcpy(&handshake[48], "-YT00000000000000000", 8);
+	memcpy(&peerId[0], "-YT00000000000000000", 20);
 
+	name = ((*infoDict)[BString::create("name")])->as<BString>()->value();
+	pieceLength = ((*infoDict)[BString::create("piece length")])->as<BInteger>()->value();
 
-	m_name = ((*infoDict)[BString::create("name")])->as<BString>()->value();
-	m_pieceLength = ((*infoDict)[BString::create("piece length")])->as<BInteger>()->value();
-
-
-	std::string pieces = ((*infoDict)[BString::create("pieces")])->as<BString>()->value();
-	for (size_t i = 0; i < pieces.size(); i += 20) {
+	std::string piecesStr = ((*infoDict)[BString::create("pieces")])->as<BString>()->value();
+	for (size_t i = 0; i < piecesStr.size(); i += 20)
+	{
 		Piece piece;
 		piece.done = false;
 		piece.priority = 0;
-		memcpy(&piece.hash[0], pieces.c_str() + i, 20);
-		m_pieces.push_back(piece);
+		memcpy(&piece.hash[0], piecesStr.c_str() + i, 20);
+		this->pieces.push_back(piece);
 	}
 
-	MKDIR(downloadDir);
+	mkdir(downloadDir.c_str(), 0700); // TODO: ADD ERROR HANDLING
 	chdir(downloadDir.c_str());
-	
+
 	std::string installDir = getcwd();
 	std::shared_ptr<BItem> &files = (*infoDict)[BString::create("files")];
-	if (files != nullptr /*i.e. there is more than 1 file*/) { // TODO: IMPLEMENT
+	if (files != nullptr /*i.e. there is more than 1 file*/)
+	{ // TODO: IMPLEMENT
 		std::cerr << "Torrent includes multiple files in it. That functionality is still in development." << std::endl;
 		return false;
 	}
-	else {
+	else
+	{
 		int64_t length = ((*infoDict)[BString::create("length")])->as<BInteger>()->value();
 
 		File file = {
-			.path = m_name.c_str(),
+			.path = name.c_str(),
 			.fp = nullptr,
 			.begin = 0,
-			.length = length
-		};
-		if (!nodeExists(m_name.c_str())) {
-			file.fp = fopen(m_name.c_str(), "wb");
-			if (!file.fp) {
-				std::cerr << m_name << ": unable to create " << m_name << std::endl;
+			.length = length};
+
+		if (!nodeExists(name.c_str())) // BRUH
+		{
+			file.fp = fopen(name.c_str(), "wb");
+			if (!file.fp)
+			{
+				std::cerr << name << ": unable to create " << name << std::endl;
 				return false;
 			}
-		} else {
-			file.fp = fopen(m_name.c_str(), "rb+");
-			if (!file.fp) {
-				std::cerr << m_name << ": unable to open " << m_name << std::endl;
+		}
+		else
+		{
+			file.fp = fopen(name.c_str(), "rb+");
+			if (!file.fp)
+			{
+				std::cerr << name << ": unable to open " << name << std::endl;
 				return false;
 			}
 
-			std::clog << m_name << ": Completed pieces: " << m_completedPieces << "/" << m_pieces.size() << std::endl;
+			std::clog << name << ": Completed pieces: " << completedPieces << "/" << pieces.size() << std::endl;
 		}
 
-		m_totalSize = length;
-		m_files.push_back(file);
+		totalSize = length;
+		this->files.push_back(file);
 	}
 
 	chdir("..");
+	return true;
+}
+
+
+TrackerQuery Torrent::buildTrackerQuery(TrackerEvent event) const // MOVE THIS FUNCTION TO TRACKER CLASS
+{
+	TrackerQuery q;
+
+	q.event = event;
+	q.uploaded = 0;
+	q.downloaded = 0;
+
+	for (size_t i = 0; i < pieces.size(); ++i)
+	{
+		if (pieces[i].done)
+		{
+			q.downloaded += pieceSize(i);
+		}
+	}
+	q.remaining = totalSize - q.downloaded;
+	return q;
+}
+
+Torrent::DownloadError Torrent::download(uint16_t port)
+{
+	size_t piecesNeeded = pieces.size();
+
+	if (!validateTracker(mainTrackerUrl, buildTrackerQuery(TrackerEvent::STARTED), port))
+	{
+		return DownloadError::TRACKER_QUERY_FAILURE;
+	}
+
+	while (completedPieces != piecesNeeded)
+	{
+		if (mainTracker->isNextRequestDue())
+		{
+			mainTracker->query(buildTrackerQuery(TrackerEvent::NONE));
+			std::cout << "\n---------------Queried again--------------" << std::endl;
+			std::cout << "Current active peers:" << activePeers.size() << std::endl
+					  << std::endl;
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+
+	for (auto &f : files)
+	{
+		// https://stackoverflow.com/a/17941712/18301773
+		fclose(f.fp);
+		f.fp = nullptr;
+	}
+
+	TrackerEvent event;
+	event = (completedPieces == piecesNeeded) ? TrackerEvent::COMPLETED : TrackerEvent::STOPPED;
+
+	mainTracker->query(buildTrackerQuery(event));
+
+	return event == TrackerEvent::COMPLETED ? DownloadError::COMPLETED : DownloadError::NETWORK_ERROR;
+}
+
+bool Torrent::validateTracker(const std::string &furl, const TrackerQuery &q, uint16_t myPort)
+{
+	UrlMeta urlMeta = parseTrackerUrl(furl);
+	if (urlMeta.host.empty())
+	{
+		std::cerr << name << ": queryTracker(): failed to parse announce url: " << furl << std::endl;
+		return false;
+	}
+
+	std::shared_ptr<Tracker> tracker(new Tracker(this, myPort, urlMeta)); // use make_shared<>
+	if (!tracker->query(q))
+	{
+		return false;
+	}
+
+	mainTracker = tracker;
 	return true;
 }
 
@@ -147,18 +220,71 @@ void Torrent::connectToPeers(const uint8_t *peers, size_t size)
 		peer->connect(parseIp(ip), std::to_string(readAsBE16(iport + 4)));
 	}
 }
-Torrent::DownloadError Torrent::download(uint16_t port)
+
+void Torrent::addPeer(const std::shared_ptr<Peer> &peer)
 {
-	size_t piecesNeeded = m_pieces.size();
-	if (m_completedPieces == piecesNeeded)
-		return DownloadError::AlreadyDownloaded;
+	activePeers.push_back(peer);
+	std::clog << name << ": Peers: " << activePeers.size() << std::endl;
+}
 
-	if(!queryTracker(m_mainTracker, makeTrackerQuery(TrackerEvent::Started), port))
-		return DownloadError::TrackerQueryFailure;
+void Torrent::removePeer(const std::shared_ptr<Peer> &peer, const std::string &errmsg)
+{
+	auto it = std::find(activePeers.begin(), activePeers.end(), peer);
+	if (it != activePeers.end())
+	{
+		activePeers.erase(it);
+	}
 
+	std::clog << name << ": " << peer->getStrIp() << ": " << errmsg << std::endl;
+	std::clog << name << ": Peers: " << activePeers.size() << std::endl;
+}
+
+void Torrent::disconnectPeers()
+{
+	for (const std::shared_ptr<Peer> &peer : activePeers)
+	{
+		peer->disconnect();
+	}
+}
+
+void Torrent::requestPiece(const std::shared_ptr<Peer> &peer)
+{
+	size_t index = 0;
+	int32_t priority = std::numeric_limits<int32_t>::max();
+
+	for (size_t i = 0; i < pieces.size(); ++i)
+	{
+		Piece *p = &pieces[i];
+		if (p->done || !peer->hasPiece(i))
+			continue;
+
+		if (!p->priority)
+		{
+			p->priority = 1;
+			return peer->sendPieceRequest(i);
+		}
+
+		if (priority > p->priority)
+		{
+			priority = p->priority;
+			index = i;
+		}
+	}
+
+	if (priority != std::numeric_limits<int32_t>::max())
+	{
+		++pieces[index].priority;
+		peer->sendPieceRequest(index);
+	}
+}
+
+void Torrent::handleTrackerError(const std::shared_ptr<Tracker> &tracker, const std::string &error)
+{
+	std::cerr << name << ": tracker request failed: " << error << std::endl;
 }
 
 void Torrent::handlePeerDebug(const std::shared_ptr<Peer> &peer, const std::string &msg)
 {
 	std::clog << name << ": " << peer->getStrIp() << " " << msg << std::endl;
 }
+
